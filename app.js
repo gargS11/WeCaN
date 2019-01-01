42596e53d4ee624ce1df00c0bd0beff63dec1db2
gargS11 <sgarg.cse16@chitkarauniversity.edu.in>
2019-01-02 00:41:43 +0530

    user register routes created

---
+++ app.js
@@ -0,0 +1,100 @@
+//see for req.flash()
+
+const express = require('express');
+const app = express();
+
+const expressValidator = require('express-validator');
+const bcrypt = require('bcryptjs');    //to hash passwords
+const session = require('express-session');
+//const cookieParser = require('cookie-parser');
+
+var User = require('./models/user');
+
+const config = require('./config/database');
+const mongoose = require('mongoose');
+mongoose.connect(config.database, { useCreateIndex: true, useNewUrlParser: true });
+let db = mongoose.connection;
+
+db.once('open', function () {
+    console.log('Connected to Mongo db');
+})
+db.on('error', function (err) {
+    console.log(err);
+});
+
+app.set('view engine', 'ejs');
+
+//middlewares
+//handle static pages routes
+app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
+// add validation methods to request
+app.use(expressValidator());
+
+//login form
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+//login process
+app.post('/login', function (req, res) {
+    res.render('admin/register');
+});
+
+//register form
+app.get('/admin/register', function (req, res) {
+    res.render('admin/register');
+});
+//register process
+app.post('/admin/register', function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const role = req.body.role;
+    console.log(req.body);
+    // *Express validator not working properly*
+    /*req.checkBody(email, 'Email is required').notEmpty();
+    req.checkBody(email, 'Email is not valid').isEmail();
+    req.checkBody(password, 'Password is required').notEmpty();
+    req.checkBody(role, 'Individual Role is required').notEmpty();
+
+    let errors = req.validationErrors();
+    console.log(errors);
+    if (errors) {
+        res.render('admin/register', { errors: errors });
+    } else {*/
+    let newUser = new User({
+        email: email,
+        password: password,
+        role: role,
+        username: email.substring(0, email.lastIndexOf("@")),
+        isProfileSet: false,
+        isDeleted: false,
+        created_at: new Date()
+    });
+
+    bcrypt.genSalt(10, function (err, salt) { //here 10 represents saltRounds
+        bcrypt.hash(newUser.password, salt, function (err, hash) {
+            if (err) {
+                console.log(err);
+            }
+            newUser.password = hash;
+            console.log(newUser);
+            newUser.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                else {
+                    console.log("User saved to database");
+                    res.redirect('/admin/register');
+                }
+            });
+        });
+    });
+});
+
+app.listen(8000);
+console.log('8000 is the magic port');
